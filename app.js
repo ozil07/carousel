c82686db509ff5ba352c1ceaca5ed52d2a94d1d3
Сайпула <ozil7@mail.ru>
2017-10-13 14:54:42 +0300

    Появилось возможность добавления кнопок навигации и кнопок (следующего и предыдущего слайда)

--- app.js
+++ app.js
@@ -1,48 +1,148 @@
-function initTabs(elem) {
-	let tabs = elem;
+function Carousel(options) {
+	let carousel = options.carousel;
+	let carouselInner = carousel.querySelector('.carousel__inner');
+	let slides = carousel.querySelectorAll('.carousel__slide');
+	let activeSlide = carousel.querySelector('.carousel__inner .active');
+	let animationDuration = options.duration || 5000;
+	let timerId;
 
-	let prevActiveTabs = tabs.querySelector('.tabs__link .active');
-	let tabsActiveContent = tabs.querySelector('.tabs__content [data-tabs-content-id="' + prevActiveTabs.getAttribute('data-tabs-content-id') + '"]');
-
-	tabsActiveContent.classList.add('active');
-
-	function showTabsContent(e) {
-		let target = e.target;
-
-		if(target.tagName != 'A') return;
-		e.preventDefault();
-
-		let str = '.tabs__content [data-tabs-content-id="' + prevActiveTabs.getAttribute('data-tabs-content-id') + '"]';
-		let prevActiveContentTabs = tabs.querySelector(str);
-
-		if(prevActiveTabs) {
-			prevActiveTabs.classList.remove('active');
-			prevActiveContentTabs.classList.remove('active');
-		}
-
-		target.classList.add('active');
-
-		let tabsContentId = target.getAttribute('data-tabs-content-id');
-		let tabsContentItem = tabs.querySelectorAll('.tabs__content__item');
-
-		for (var i = tabsContentItem.length - 1; i >= 0; i--) {
-			if(tabsContentItem[i].getAttribute('data-tabs-content-id') == tabsContentId) {
-				tabsContentItem[i].classList.add('active');
-			}
-		};
-
-		prevActiveTabs = target;
-
+	
+	function setCarouselHeight() {
+		carousel.style.height = activeSlide.offsetHeight + 'px';
 	}
 
-	tabs.addEventListener('click', showTabsContent);
-	
+	function hightNextNavs() {
+		let carouselNavsItem = carousel.querySelectorAll('.carousel__navs span');
+		let prevNavsItem;
+
+		for (var i = 0; i < slides.length; i++) {
+
+			if(slides[i] == activeSlide) {
+				carouselNavsItem[i].classList.add('active');
+				if(i > 0) prevNavsItem = i - 1;
+			};
+
+			if(i == 0) prevNavsItem = slides.length - 1;
+
+		};
+
+		if(carouselNavsItem[prevNavsItem].classList.contains('active')) {
+			carouselNavsItem[prevNavsItem].classList.remove('active');
+		}
+	}
+
+
+	function hightPrevNavs() {
+		let carouselNavsItem = carousel.querySelectorAll('.carousel__navs span');
+		let prevNavsItem;
+
+		for (var i = 0; i < slides.length; i++) {
+
+			if(slides[i] == activeSlide) {
+				carouselNavsItem[i].classList.add('active');
+				if(i == slides.length - 1) {
+					prevNavsItem = 0;
+				} else {
+					prevNavsItem = i + 1;
+					carouselNavsItem[prevNavsItem].classList.remove('active');
+				};
+			};
+		};
+
+		carouselNavsItem[prevNavsItem].classList.remove('active');
+		
+	}
+
+	function nextSlide() {
+		if(!activeSlide.nextElementSibling) {
+			activeSlide.classList.remove('active');
+			carouselInner.firstElementChild.classList.add('active');
+			activeSlide = carouselInner.firstElementChild;
+		} else {
+			activeSlide.classList.remove('active');
+			activeSlide.nextElementSibling.classList.add('active');
+			activeSlide = activeSlide.nextElementSibling;
+		}
+		hightNextNavs();
+		setCarouselHeight();
+	}
+
+	function prevSlide() {
+		if(!activeSlide.previousElementSibling) {
+			activeSlide.classList.remove('active');
+			carouselInner.lastElementChild.classList.add('active');
+			activeSlide = carouselInner.lastElementChild;
+		} else {
+			activeSlide.classList.remove('active');
+			activeSlide.previousElementSibling.classList.add('active');
+			activeSlide = activeSlide.previousElementSibling;
+		}
+		hightPrevNavs();
+		setCarouselHeight();
+	}
+
+	function autoPlayCarousel() {
+		timerId = setInterval(function() {
+			nextSlide();
+		}, animationDuration);
+	}
+
+	function initCarouselNavs(arr) {
+		let slides = arr;
+		let navsCarousel = document.createElement('div');
+
+		navsCarousel.className = 'carousel__navs';
+
+		for (var i = slides.length - 1; i >= 0; i--) {
+			let span = document.createElement('span');
+			navsCarousel.appendChild(span);
+		};
+
+		carousel.appendChild(navsCarousel);
+	}
+
+	function initCarouselControl() {
+		let carouselControl = document.createElement('div');
+		let prev = document.createElement('div');
+		let next = document.createElement('div');
+
+		carouselControl.className = 'carousel__control';
+		prev.className = 'carousel__control__prev';
+		next.className = 'carousel__control__next';
+
+		prev.textContent = '←';
+		next.textContent = '→';
+
+		carouselControl.appendChild(prev);
+		carouselControl.appendChild(next);
+
+		prev.addEventListener('click', function(e){
+			prevSlide();
+			clearInterval(timerId);
+			autoPlayCarousel();
+		});
+
+		next.addEventListener('click', function(e){
+			nextSlide();
+			clearInterval(timerId);
+			autoPlayCarousel();
+		});
+
+		carousel.appendChild(carouselControl);
+	}
+
+	setCarouselHeight();
+	autoPlayCarousel();
+	initCarouselNavs(slides);
+	initCarouselControl();
+	hightNextNavs();
 }
 
-let tabs = document.querySelectorAll('.tabs')
+let carouselArr = document.querySelectorAll('.carousel');
 
-for (var i = tabs.length - 1; i >= 0; i--) {
-	initTabs(tabs[i]);
+for (var i = carouselArr.length - 1; i >= 0; i--) {
+	new Carousel({
+		carousel: carouselArr[i],
+	})
 };
-
 
